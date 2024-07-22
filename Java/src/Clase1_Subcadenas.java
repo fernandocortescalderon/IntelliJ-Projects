22993962a287a76ac09b0c9f25b9e21be5e1853e
Fernando Cortés <fernando@cortescalderon.com>
2024-07-22 14:43:16 -0600

    Subcadenas

---
+++ Java/src/Clase1_Subcadenas.java
@@ -0,0 +1,12 @@
+public class Clase1_Subcadenas {
+    public static void main(String[] args) {
+        // Obtiene una parte de una cadena
+        // El último indice no se incluye
+
+        // Cadena original
+        var cadena = "hola mundo";
+
+        // Subcadena
+        System.out.println(cadena.substring(0, 4));
+    }
+}
