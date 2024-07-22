f2fbe483fa37982ace1e7c4718f637af506ec1f4
Fernando Cortés <fernando@cortescalderon.com>
2024-07-22 11:28:43 -0600

    Indices de cadena

---
+++ Java/src/Clase1_IndicesDeCadena.java
@@ -0,0 +1,8 @@
+public class Clase1_IndicesDeCadena {
+    public static void main(String[] args) {
+        var cadena = "Hola mundo";
+
+        var primerCaracter = cadena.charAt(0);
+        System.out.println(primerCaracter);
+    }
+}
