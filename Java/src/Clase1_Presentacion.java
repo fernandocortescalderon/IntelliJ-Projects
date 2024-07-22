1a056b40d52f18f45887844e4fe5db815889c3ec
Fernando Cortés <fernando@cortescalderon.com>
2024-07-22 10:33:38 -0600

    Clase 1: Presentación

---
+++ Java/src/Clase1_Presentacion.java
@@ -0,0 +1,7 @@
+public class Clase1_Presentacion {
+    public static void main(String[] args) {
+        System.out.println("Nombre: Ferando Cortés");
+        System.out.println("Edad: 31 años");
+        System.out.println("País: México");
+    }
+}
