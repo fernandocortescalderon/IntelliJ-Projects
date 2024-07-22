ccb445dd1fbdf8ab09a6e8e0ad92165600e44cf9
Fernando Cortés <fernando@cortescalderon.com>
2024-07-22 11:04:29 -0600

    Constantes

---
+++ Java/src/Clase1_Constantes.java
@@ -0,0 +1,6 @@
+public class Clase1_Constantes {
+    public static void main(String[] args) {
+        // Sintaxis: Se usa la palabra final, tipo, nombre en mayúsculas, valor
+        final var NOMBRE_VARIABLE = "Fernando";
+    }
+}
