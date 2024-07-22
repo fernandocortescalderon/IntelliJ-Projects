55b2d72538a030fc632d744755a01454b1b389f5
Fernando Cortés <fernando@cortescalderon.com>
2024-07-22 10:43:19 -0600

    Variables

---
+++ Java/src/Variables.java
@@ -0,0 +1,9 @@
+public class Variables {
+    public static void main(String[] args) {
+        int edad = 31;
+        double precio = 300.40;
+        boolean disponible = true;
+        char genero;
+        genero = 'M';
+    }
+}
