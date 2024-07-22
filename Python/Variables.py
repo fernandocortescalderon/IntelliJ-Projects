8e000fa8dae8ff7e5038d6d5e01e0b983fbcbc0b
Fernando Cortés <fernando@cortescalderon.com>
2024-07-22 17:23:02 -0600

    Variables

---
+++ Python/Variables.py
@@ -0,0 +1,12 @@
+miVariable = "Fernando"
+print(miVariable)
+
+# Ejercicio
+
+nombre = "Fernando"
+telefono = 5632655043
+email = "fernando@cortescalderon.com"
+
+print("nombre: " + nombre)
+print("telefono: " + str(telefono))
+print("email: " + email)
