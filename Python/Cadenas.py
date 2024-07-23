207f419dcf8c95ec6d5821039995b1619f99376b
Fernando Cortés <fernando@cortescalderon.com>
2024-07-22 18:48:01 -0600

    Cadenas

---
+++ Python/Cadenas.py
@@ -0,0 +1,8 @@
+# Cadenas
+
+miGrupoFavorito = "Maneskin"
+
+print("Mi grupo favorito es " + miGrupoFavorito)
+
+print("Mi grupo favorito es:", miGrupoFavorito) # Agrega un espacio en blanco de manera automática
+
