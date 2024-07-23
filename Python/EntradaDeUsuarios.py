3dcea3fedeafe502b4bd9272bb7649974c25a27e
Fernando Cortés <fernando@cortescalderon.com>
2024-07-22 19:47:33 -0600

    Entrada de Usuarios

---
+++ Python/EntradaDeUsuarios.py
@@ -0,0 +1,9 @@
+numero1 = int(input("Escribe el primer número: "))
+numero2 = int(input("Escribe un segundo número: "))
+resultado = numero1 + numero2
+
+print("El resultado de ambos números es:", resultado)
+
+# Ejercicio
+dia = int(input("¿Cómo estuvo tu día? (del 1 al 10):"))
+print("Mi día estuvo de:", dia)
