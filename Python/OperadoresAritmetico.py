f511414ef9cebf45e429b7f6d56e949c7416cb58
Fernando Cortés <fernando@cortescalderon.com>
2024-07-22 20:11:31 -0600

    Operadores aritméticos

---
+++ Python/OperadoresAritmetico.py
@@ -0,0 +1,15 @@
+operadorA = 3
+operadorB = 2
+suma = operadorA + operadorB
+resta = operadorA - operadorB
+multiplicacion = operadorA * operadorB
+division = operadorA / operadorB
+modulo = operadorA % operadorB
+exponente = operadorA ** operadorB
+
+print(f'Resultado suma: {suma}')
+print(f'Resultado resta: {resta}')
+print(f'Resultado multiplicacion: {multiplicacion}')
+print(f'Resultado division: {division}')
+print(f'Resultado modulo: {modulo}')
+print(f'Resultado exponente: {exponente}')
